c124a1b3be50bfcb1cd3a3c71f7996d518b73be6
ha752002 <honggha75@gmail.com>
2023-11-19 13:24:57 +0700

    fix UI

--- src/pages/Home/components/Column.jsx
+++ src/pages/Home/components/Column.jsx
@@ -2,16 +2,21 @@ import React from 'react';
 import {Draggable, Droppable} from "react-beautiful-dnd";
 import Task from "./Task.jsx";
 import clsx from "clsx";
-import  Styles from "./Column.module.scss"
+import Styles from "./Column.module.scss"
+import trashIcon from "../../../assets/images/trash.svg";
 
 function Column({column, index}) {
     return (<>
         <Draggable draggableId={column.column} index={index}>
-            {(provided) => (<div className={clsx(Styles.column_wrapper)} key={column.column} ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
-                <p>{column.column}</p>
+            {(provided) => (<div className={clsx(Styles.column_wrapper)} key={column.column}
+                                 ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+
+                <div className={clsx(Styles.column_title)}>{column.column}
+                    <i className={clsx(Styles.trash_icon, 'fa', 'fa-solid', 'fa-trash')}></i>
+                </div>
                 <Droppable droppableId={column.column} type="task">
                     {(provided) => (
-                        <ul  className={clsx(Styles.characters)}  {...provided.droppableProps}
+                        <ul className={clsx(Styles.characters)}  {...provided.droppableProps}
                             ref={provided.innerRef}>
                             {column.tasks && column.tasks.map((task, index) => {
                                 return <Task task={task} index={index} key={task._id}></Task>
--- src/pages/Home/components/Task.jsx
+++ src/pages/Home/components/Task.jsx
@@ -2,17 +2,14 @@ import React from 'react';
 import {Draggable} from "react-beautiful-dnd";
 
 function Task({task, index}) {
-    return (
-        <Draggable draggableId={task._id}
-                   index={index}>
-            {(provided) => (
-                <li ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
-                    <p>
-                        {task.content}
-                    </p>
-                </li>)}
-        </Draggable>
-    );
+    return (<Draggable draggableId={task._id}
+                       index={index}>
+        {(provided) => (<li ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
+            <p>
+                {task.content}
+            </p>
+        </li>)}
+    </Draggable>);
 }
 
 export default Task;
